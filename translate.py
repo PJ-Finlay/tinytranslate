892e7dab4fbdcd2b45f977eacf77a5cb9cae3631
P.J. Finlay <pjappdevelopment@gmail.com>
2022-07-06 14:36:07 +0000

    Print predictions

--- translate.py
+++ translate.py
@@ -11,26 +11,7 @@ from extra.training import train, evaluate
 from models.transformer import Transformer
 from tinygrad.optim import Adam
 
-# dataset idea from https://github.com/karpathy/minGPT/blob/master/play_math.ipynb
-def make_dataset():
-    ds = []
-    for i in range(100):
-        for j in range(100):
-            s = i + j
-            ds.append(
-                [i // 10, i % 10, j // 10, j % 10, s // 100, (s // 10) % 10, s % 10]
-            )
-    random.shuffle(ds)
-    ds = np.array(ds)
-    ds_X = ds[:, 0:6]
-    ds_Y = np.copy(ds[:, 1:])
-    ds_X_train, ds_X_test = ds_X[0:8000], ds_X[8000:]
-    ds_Y_train, ds_Y_test = ds_Y[0:8000], ds_Y[8000:]
-
-    return ds_X_train, ds_Y_train, ds_X_test, ds_Y_test
-
-
-MAX_LEN = 6
+MAX_LEN = 20
 NUM_CLASSES = 255
 
 def int_of_char(c):
@@ -41,6 +22,12 @@ def int_of_char(c):
 
 def ints_of_str(s):
     return [int_of_char(c) for c in s]
+
+def str_of_ints(ints):
+    to_return = ''
+    for i in ints:
+        to_return += chr(i)
+    return to_return
 
 def make_translation_dataset():
     """
@@ -105,18 +92,7 @@ if __name__ == "__main__":
         )
         lr /= 1.2
         print(f"reducing lr to {lr:.4f}")
-        if acc > 0.998:
-            wrong = 0
-            for k in range(len(Y_test_preds)):
-                if (Y_test_preds[k] != Y_test[k]).any():
-                    wrong += 1
-                    a, b, c, x = (
-                        X_test[k, :2],
-                        X_test[k, 2:4],
-                        Y_test[k, -3:],
-                        Y_test_preds[k, -3:],
-                    )
-                    print(
-                        f"{a[0]}{a[1]} + {b[0]}{b[1]} = {x[0]}{x[1]}{x[2]} (correct: {c[0]}{c[1]}{c[2]})"
-                    )
-            print(f"Wrong predictions: {wrong}, acc = {acc:.4f}")
+        k = random.randint(0, len(Y_test_preds))
+        print("Source: " + str_of_ints(X_test[k]))
+        print("Target: " + str_of_ints(Y_test[k]))
+        print("Pred:   " + str_of_ints(Y_test_preds[k]))
